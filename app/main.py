023be601e79f84834cc8d6ff0f4cef4d3f602140
Bohdana Ostrovska <bogdanaostrovska986@gmail.com>
2023-07-10 22:11:13 +0300

    Change fastreload function

--- app/main.py
+++ app/main.py
@@ -1,13 +1,4 @@
-from dotenv import load_dotenv
-from pathlib import Path
-import os
 from fastapi import FastAPI
-
-env_path = Path(__file__).resolve().parent.parent / '.env'
-load_dotenv(dotenv_path=env_path)
-print(env_path)
-server_port = os.getenv('SERVER_PORT')
-print(server_port)
 
 app = FastAPI()
 
--- run.py
+++ run.py
@@ -1,11 +1,10 @@
+import os
 import uvicorn
-from watchgod import watch
+from dotenv import load_dotenv
 
+from app.main import app
 
-def run():
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
-
+load_dotenv()
 
 if __name__ == "__main__":
-    for changes in watch("."):
-        run()
+    uvicorn.run(app, host=os.getenv('SERVER_HOST'), port=int(os.getenv('SERVER_PORT')), reload=True)
