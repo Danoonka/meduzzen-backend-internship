336d7e1e8e02d7e6e4ef1606564a51af8c27d40c
Bohdana Ostrovska <bogdanaostrovska986@gmail.com>
2023-08-15 13:35:12 +0300

    Refactor

--- app/routers/quiz.py
+++ app/routers/quiz.py
@@ -19,6 +19,23 @@ async def get_quiz_service(session: AsyncSession = Depends(get_session)) -> Quiz
     return QuizService(session)
 
 
+def authorize_quiz_access(quiz_id_param_name: str):
+    async def _authorize(quiz_id: int, current_user: FullUserResponse = Depends(get_current_user),
+                         actions_service: ActionsService = Depends(get_action_service),
+                         company_service: CompanyService = Depends(get_company_service),
+                         quiz_service: QuizService = Depends(get_quiz_service)):
+        quiz = await quiz_service.get_quiz_by_id(quiz_id=quiz_id)
+        company = await company_service.get_company_by_id(company_id=quiz.company_id)
+        admins = await actions_service.get_all_admins(company_id=company.company_id)
+        is_admin = any(admin.get('user_id') == current_user.result.user_id for admin in admins)
+        if company.owner_id == current_user.result.user_id or is_admin:
+            return quiz, company
+        else:
+            raise HTTPException(status_code=403, detail="Unauthorized")
+
+    return _authorize
+
+
 @quiz_router.post('/create-quiz/company/{company_id}/', response_model=FullQuizResponse)
 async def create_quiz_endpoint(company_id: int,
                                quiz_data: QuizToCreate, current_user: FullUserResponse = Depends(get_current_user),
@@ -34,7 +51,7 @@ async def create_quiz_endpoint(company_id: int,
         for question in quiz_data.question_list:
             if len(question.question_answers) < 2:
                 raise HTTPException(status_code=400, detail="Each question must have at least 2 answers.")
-        quiz = await quiz_service.create_quiz(quizData=quiz_data, company_id=company_id,
+        quiz = await quiz_service.create_quiz(quiz_data=quiz_data, company_id=company_id,
                                               user_id=current_user.result.user_id)
         return toFullQuizResponse(quiz=quiz, status_code=200, detail="Quiz created successfully")
     else:
@@ -42,42 +59,26 @@ async def create_quiz_endpoint(company_id: int,
 
 
 @quiz_router.put('/update-quiz/{quiz_id}/', response_model=FullQuizResponse)
-async def update_quiz_endpoint(quiz_id: int, quiz_data: QuizToUpdate,
-                               current_user: FullUserResponse = Depends(get_current_user),
-                               actions_service: ActionsService = Depends(get_action_service),
-                               company_service: CompanyService = Depends(get_company_service),
-                               quiz_service: QuizService = Depends(get_quiz_service)
-                               ) -> FullQuizResponse:
-    quiz_to_update = await quiz_service.get_quiz_by_id(quiz_id=quiz_id)
-    company = await company_service.get_company_by_id(company_id=quiz_to_update.company_id)
-    admins = await actions_service.get_all_admins(company_id=company.company_id)
-    is_admin = any(admin.get('user_id') == current_user.result.user_id for admin in admins)
-    if company.owner_id == current_user.result.user_id or is_admin:
-        quiz = await quiz_service.update_quiz(quiz=quiz_to_update, quiz_data=quiz_data)
-        return toFullQuizResponse(quiz=quiz, status_code=200, detail="Quiz updated successfully")
-    else:
-        raise HTTPException(status_code=403, detail="Unauthorized")
+async def update_quiz_endpoint(quiz_data: QuizToUpdate,
+                               authorized_data=Depends(authorize_quiz_access('quiz_id')),
+                               quiz_service: QuizService = Depends(get_quiz_service)) -> FullQuizResponse:
+    quiz_to_update, company = authorized_data
+    updated_quiz = await quiz_service.update_quiz(quiz=quiz_to_update, quiz_data=quiz_data)
+    return toFullQuizResponse(quiz=updated_quiz, status_code=200, detail="Quiz updated successfully")
 
 
 @quiz_router.delete('/delete-quiz/{quiz_id}', response_model=DeleteQuizResponse)
 async def delete_quiz_endpoint(quiz_id: int,
-                               current_user: FullUserResponse = Depends(get_current_user),
-                               actions_service: ActionsService = Depends(get_action_service),
-                               company_service: CompanyService = Depends(get_company_service),
-                               quiz_service: QuizService = Depends(get_quiz_service)) -> DeleteQuizResponse:
-    quiz_to_delete = await quiz_service.get_quiz_by_id(quiz_id=quiz_id)
-    company = await company_service.get_company_by_id(company_id=quiz_to_delete.company_id)
-    admins = await actions_service.get_all_admins(company_id=company.company_id)
-    is_admin = any(admin.get('user_id') == current_user.result.user_id for admin in admins)
-    if company.owner_id == current_user.result.user_id or is_admin:
-        quiz_id = await quiz_service.delete_quiz(quiz_id=quiz_id)
-        return DeleteQuizResponse(
-            status_code=200,
-            detail="Quiz deleted successfully",
-            result=quiz_id
-        )
-    else:
-        raise HTTPException(status_code=403, detail="Unauthorized")
+                               authorized_data=Depends(authorize_quiz_access('quiz_id')),
+                               quiz_service: QuizService = Depends(get_quiz_service)
+                               ) -> DeleteQuizResponse:
+    quiz_to_delete, _ = authorized_data
+    deleted_quiz_id = await quiz_service.delete_quiz(quiz_id=quiz_id)
+    return DeleteQuizResponse(
+        status_code=200,
+        detail="Quiz deleted successfully",
+        result=deleted_quiz_id
+    )
 
 
 @quiz_router.get('/all-company-quiz/company/{company_id}', response_model=QuizListResponse)
@@ -92,60 +93,46 @@ async def get_all_company_quiz_endpoint(company_id: int,
 
 
 @quiz_router.post('/add-new-question/{quiz_id}', response_model=FullQuizResponse)
-async def add_question_endpoint(quiz_id: int, questionData: QuestionBase,
-                                current_user: FullUserResponse = Depends(get_current_user),
-                                actions_service: ActionsService = Depends(get_action_service),
-                                company_service: CompanyService = Depends(get_company_service),
-                                quiz_service: QuizService = Depends(get_quiz_service)) -> FullQuizResponse:
-    quiz = await quiz_service.get_quiz_by_id(quiz_id=quiz_id)
-    company = await company_service.get_company_by_id(company_id=quiz.company_id)
-    admins = await actions_service.get_all_admins(company_id=company.company_id)
-    is_admin = any(admin.get('user_id') == current_user.result.user_id for admin in admins)
-    if company.owner_id == current_user.result.user_id or is_admin:
-        await quiz_service.add_question(quiz_id=quiz_id, question_data=questionData)
-        return toFullQuizResponse(quiz=quiz, status_code=200, detail="Quiz updated successfully")
-    else:
-        raise HTTPException(status_code=403, detail="Unauthorized")
+async def add_question_endpoint(quiz_id: int, question_data: QuestionBase,
+                                authorized_data=Depends(authorize_quiz_access('quiz_id')),
+                                quiz_service: QuizService = Depends(get_quiz_service)
+                                ) -> FullQuizResponse:
+    quiz, _ = authorized_data
+    await quiz_service.add_question(quiz_id=quiz_id, question_data=question_data)
+    return toFullQuizResponse(quiz=quiz, status_code=200, detail="Quiz updated successfully")
 
 
 @quiz_router.put('/update-question/quiz/{quiz_id}/question/{question_id}/', response_model=FullQuizResponse)
-async def update_question_endpoint(question_id: int, quiz_id: int,
-                                   questionData: QuestionBase,
-                                   current_user: FullUserResponse = Depends(get_current_user),
-                                   actions_service: ActionsService = Depends(get_action_service),
-                                   company_service: CompanyService = Depends(get_company_service),
+async def update_question_endpoint(question_id: int, question_data: QuestionBase,
+                                   authorized_data=Depends(authorize_quiz_access('quiz_id')),
                                    quiz_service: QuizService = Depends(get_quiz_service)
                                    ) -> FullQuizResponse:
-    quiz = await quiz_service.get_quiz_by_id(quiz_id=quiz_id)
-    company = await company_service.get_company_by_id(company_id=quiz.company_id)
-    admins = await actions_service.get_all_admins(company_id=company.company_id)
-    is_admin = any(admin.get('user_id') == current_user.result.user_id for admin in admins)
-    if company.owner_id == current_user.result.user_id or is_admin:
-        if len(questionData.question_answers) < 2:
-            raise Exception("Question can`t consist with less then 2 answers")
-        await quiz_service.update_question(question_id=question_id, question_data=questionData)
-        return toFullQuizResponse(quiz=quiz, status_code=200, detail="Quiz updated successfully")
-    else:
-        raise HTTPException(status_code=403, detail="Unauthorized")
+    quiz, _ = authorized_data
+    if len(question_data.question_answers) < 2:
+        raise HTTPException(status_code=400, detail="Question must have at least 2 answers")
+    await quiz_service.update_question(question_id=question_id, question_data=question_data)
+    return toFullQuizResponse(quiz=quiz, status_code=200, detail="Quiz updated successfully")
 
 
 @quiz_router.delete('/delete-question/quiz/{quiz_id}/question/{question_id}', response_model=DeleteQuizResponse)
-async def delete_question_endpoint(quiz_id: int,
-                                   question_id: int,
-                                   current_user: FullUserResponse = Depends(get_current_user),
-                                   actions_service: ActionsService = Depends(get_action_service),
-                                   company_service: CompanyService = Depends(get_company_service),
-                                   quiz_service: QuizService = Depends(get_quiz_service)) -> DeleteQuizResponse:
-    quiz = await quiz_service.get_quiz_by_id(quiz_id=quiz_id)
-    company = await company_service.get_company_by_id(company_id=quiz.company_id)
-    admins = await actions_service.get_all_admins(company_id=company.company_id)
-    is_admin = any(admin.get('user_id') == current_user.result.user_id for admin in admins)
-    if company.owner_id == current_user.result.user_id or is_admin:
-        await quiz_service.delete_question(question_id=question_id, quiz_id=quiz_id)
-        return DeleteQuizResponse(
-            status_code=200,
-            detail="Question deleted successfully",
-            result=quiz_id
-        )
-    else:
-        raise HTTPException(status_code=403, detail="Unauthorized")
+async def delete_question_endpoint(quiz_id: int, question_id: int,
+                                   authorized_data=Depends(authorize_quiz_access('quiz_id')),
+                                   quiz_service: QuizService = Depends(get_quiz_service)
+                                   ) -> DeleteQuizResponse:
+    quiz, _ = authorized_data
+    await quiz_service.delete_question(question_id=question_id, quiz_id=quiz_id)
+    return DeleteQuizResponse(
+        status_code=200,
+        detail="Question deleted successfully",
+        result=question_id
+    )
+
+
+@quiz_router.get('/quiz/{quiz_id}', response_model=FullQuizResponse)
+async def get_quiz_by_id_endpoint(authorized_data=Depends(authorize_quiz_access('quiz_id'))) -> FullQuizResponse:
+    quiz, _ = authorized_data
+    return toFullQuizResponse(
+        status_code=200,
+        detail="Get quiz successfully",
+        quiz=quiz
+    )
--- app/services/quiz.py
+++ app/services/quiz.py
@@ -9,77 +9,82 @@ class QuizService:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create_quiz(self, quizData: QuizToCreate, company_id: int, user_id: int) -> QuizBase:
+    async def create_quiz(self, quiz_data: QuizToCreate, company_id: int, user_id: int) -> QuizBase:
         quiz = Quiz(
-            quiz_name=quizData.quiz_name,
-            quiz_frequency=quizData.quiz_frequency,
+            quiz_name=quiz_data.quiz_name,
+            quiz_frequency=quiz_data.quiz_frequency,
             company_id=company_id,
             created_by=user_id
         )
 
         self.session.add(quiz)
         await self.session.flush()
-        for question_data in quizData.question_list:
-            question = Question(
-                quiz_id=quiz.quiz_id,
-                question_text=question_data.question_text,
-                question_answers=question_data.question_answers,
-                question_correct_answer=question_data.question_correct_answer
-            )
-            self.session.add(question)
+
+        for question_data in quiz_data.question_list:
+            await self._create_question(quiz.quiz_id, question_data)
+
         await self.session.commit()
-
         return quiz
 
-    async def get_quiz_by_id(self, quiz_id: int) -> QuizBase:
-        quiz = await self.session.scalar(
-            select(Quiz).where(Quiz.quiz_id == quiz_id)
-        )
+    async def update_quiz(self, quiz: Quiz, quiz_data: QuizToUpdate) -> QuizBase:
+        for field, value in quiz_data.dict(exclude_unset=True).items():
+            setattr(quiz, field, value)
+
+        await self.session.flush()
+        await self.session.commit()
         return quiz
-
-    async def get_question_by_id(self, question_id: int) -> QuestionBase:
-        question = await self.session.scalar(
-            select(Question).where(Question.question_id == question_id)
-        )
-        return question
-
-    async def update_quiz(self, quiz, quiz_data: QuizToUpdate) -> QuizBase:
-        quiz = await self.get_quiz_by_id(quiz_id=quiz.quiz_id)
-        if quiz:
-            for field, value in quiz_data.dict(exclude_unset=True).items():
-                setattr(quiz, field, value)
-            await self.session.flush()
-            await self.session.commit()
-            return quiz
 
     async def delete_quiz(self, quiz_id: int) -> int:
-        quiz = await self.get_quiz_by_id(quiz_id=quiz_id)
+        quiz = await self.get_quiz_by_id(quiz_id)
 
         if quiz:
-            questions = await self.session.execute(
-                select(Question).where(Question.quiz_id == quiz_id))
-
-            question_instances = [question[0] for question in questions]
-
-            for question in question_instances:
-                await self.session.delete(question)
-
+            await self._delete_questions_by_quiz_id(quiz_id)
             await self.session.delete(quiz)
             await self.session.commit()
             return quiz.quiz_id
         else:
             raise Exception("Quiz does not exist")
 
-    async def get_all_company_quiz(self, company_id: int) -> list[QuizBase]:
-        quizzes = await self.session.execute(
-            select(Quiz).where(Quiz.company_id == company_id)
-        )
-
-        all_quizzes = [QuizBase(**quiz.__dict__) for quiz in quizzes.scalars().all()]
-
-        return all_quizzes
-
     async def add_question(self, quiz_id, question_data: QuestionBase) -> QuestionBase:
+        question = await self._create_question(quiz_id, question_data)
+        await self.session.commit()
+        return question
+
+    async def update_question(self, question_id: int, question_data: QuestionBase) -> QuestionBase:
+        question = await self.get_question_by_id(question_id=question_id)
+        for field, value in question_data.dict(exclude_unset=True).items():
+            setattr(question, field, value)
+
+        await self.session.flush()
+        await self.session.commit()
+        return question
+
+    async def delete_question(self, question_id: int, quiz_id: int) -> int:
+        question = await self.get_question_by_id(question_id=question_id)
+        questions_count = await self._get_question_count_by_quiz_id(quiz_id)
+
+        if questions_count <= 2:
+            raise Exception("Quiz must have at least 2 questions")
+
+        if question:
+            await self.session.delete(question)
+            await self.session.flush()
+            await self.session.commit()
+            return question.quiz_id
+        else:
+            raise Exception("Question does not exist")
+
+    async def get_quiz_by_id(self, quiz_id: int) -> QuizBase:
+        return await self.session.scalar(select(Quiz).where(Quiz.quiz_id == quiz_id))
+
+    async def get_question_by_id(self, question_id: int) -> QuestionBase:
+        return await self.session.scalar(select(Question).where(Question.question_id == question_id))
+
+    async def get_all_company_quiz(self, company_id: int) -> list[QuizBase]:
+        quizzes = await self.session.execute(select(Quiz).where(Quiz.company_id == company_id))
+        return [QuizBase(**quiz.__dict__) for quiz in quizzes.scalars().all()]
+
+    async def _create_question(self, quiz_id, question_data: QuestionBase) -> QuestionBase:
         question = Question(
             quiz_id=quiz_id,
             question_text=question_data.question_text,
@@ -91,30 +96,12 @@ class QuizService:
             raise Exception("Add more answers")
 
         self.session.add(question)
-        await self.session.commit()
         return question
 
-    async def update_question(self, question_id: int, question_data: QuestionBase) -> QuestionBase:
-        question = await self.get_question_by_id(question_id=question_id)
-        if question:
-            for field, value in question_data.dict(exclude_unset=True).items():
-                setattr(question, field, value)
-            await self.session.flush()
-            await self.session.commit()
-            return question
-
-    async def delete_question(self, question_id: int, quiz_id: int) -> int:
-        question = await self.get_question_by_id(question_id=question_id)
-        questions = await self.session.execute(
-            select(Question).where(Question.quiz_id == quiz_id))
-
-        question_instances = [question[0] for question in questions]
-        if len(question_instances) == 2:
-            raise ("Quiz can`t consist less then 2 question")
-        if question:
+    async def _delete_questions_by_quiz_id(self, quiz_id: int):
+        questions = await self.session.execute(select(Question).where(Question.quiz_id == quiz_id))
+        for question in questions.scalars().all():
             await self.session.delete(question)
-            await self.session.flush()
-            await self.session.commit()
-            return question.quiz_id
-        else:
-            raise Exception("Question does not exist")
+
+    async def _get_question_count_by_quiz_id(self, quiz_id: int) -> int:
+        return await self.session.execute(select(Question).where(Question.quiz_id == quiz_id)).scalar()
